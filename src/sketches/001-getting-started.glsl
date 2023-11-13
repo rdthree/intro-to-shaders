7faecf730eebc14a8d1a09338bdb754560b38d52
rdthree <1165540+rdthree@users.noreply.github.com>
2023-11-12 21:52:53 -0500

    completed getting started

--- src/sketches/001-getting-started.glsl
+++ src/sketches/001-getting-started.glsl
@@ -3,9 +3,24 @@ precision highp float;
 varying vec2 vUv;
 uniform float uTime;
 
-// The most basic shader
 void main(){
-  vec3 color = vec3(1.);
-
-  gl_FragColor = vec4(color, 1.0);
+    vec3 color = vec3(0.0);
+    vec2 uv = vUv;
+    
+    //horizontal bw gradient
+    //color = vec3(uv.x);
+    
+    //vertical bw gradient
+    //color = vec3(uv.y);
+    
+    //pulsate!
+    //color = vec3(fract(uTime));
+    
+    // red half blue half
+    color = vec3(1.0, 0.0, 0.0);
+    if (vUv.x > 0.5) {
+        color = vec3(0.0, 0.0, 1.0);
+    }
+    
+    gl_FragColor = vec4(color, 1.0);
 }
