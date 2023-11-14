7f78d95a84330708164aad3d96dbf6246a518a6a
rdthree <1165540+rdthree@users.noreply.github.com>
2023-11-13 21:24:34 -0500

    about to use the normalization function

--- src/sketches/004-sine-and-cosine.glsl
+++ src/sketches/004-sine-and-cosine.glsl
@@ -3,9 +3,27 @@ precision highp float;
 varying vec2 vUv;
 uniform float uTime;
 
-void main(){
-  vec3 color = vec3(1.);
+// transformation: reduces amplitude to 0.5, then move the whole wave into 0 to 1
+float normalSin(float val) {
+  return sin(val) * 0.5 + 0.5;
+}
 
-  gl_FragColor = vec4(color, 1.0);
+void main(){
+  vec2 uv = vUv;
+  vec3 color = vec3(1.);
+  
+  // transformation: reduces amplitude to 0.5, then move the whole wave into 0 to 1
+  float edge = sin(uTime) * 0.5 + 0.5;
+  
+  float gradient = 0.0;
+  // angle
+  //gradient = step(uv.y - sin(uTime), uv.x);
+  // horizontal
+  //gradient = step(sin(uTime), uv.x);
+  //vertical
+  gradient = step(uv.y, sin(uTime));
+  vec3 result = vec3(gradient);
+
+  gl_FragColor = vec4(result, 1.0);
 }
 
